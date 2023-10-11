aed3de7a4f53344cebb45d49495f875772621e20
Illia-creator <sie29092002@gmail.com>
2023-10-11 22:13:59 +0300

    Rewrote File Locations

--- KP-Lab2/KP-Lab2/Dtos/IncomeDataDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace KP_Lab2.Dtos;
-
-public class IncomeDataDto
-{
-    public int[] NumbersLineParameters { get; set; }
-    public int[] NumbersToFind { get; set; }
-}
--- KP-Lab2/KP-Lab2/Exceptions/CustomException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KP_Lab2.Exceptions;
-
-public class CustomException : Exception
-{
-    public string Message { get; set; } = string.Empty;
-
-    public CustomException(string message)
-       => Message = message;
-}
