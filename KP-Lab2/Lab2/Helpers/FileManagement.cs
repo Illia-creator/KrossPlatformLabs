ff87e5c7c3108cb201a91ed5e1462a6b13178324
Illia-creator <sie29092002@gmail.com>
2023-10-11 22:05:57 +0300

    Rewrote File Locations

---
+++ KP-Lab2/Lab2/Helpers/FileManagement.cs
@@ -0,0 +1,56 @@
+﻿namespace Lab2.Helpers;
+
+public static class FileManagement
+{
+    public static string GetDataFromFile(string path)
+    {
+
+        if (!File.Exists(path))
+            throw new CustomException("File not found");
+
+        string fileContents = File.ReadAllText(path);
+
+        string modifiedContents = fileContents.Replace(Environment.NewLine, " ");
+
+        return modifiedContents;
+    }
+
+    public static void WriteAnswer(int[] numbers)
+    {
+        string fileName;
+        bool fileExists;
+
+        do
+        {
+            Console.Write("Enter the file name: ");
+            fileName = Console.ReadLine();
+            fileExists = File.Exists(fileName);
+
+            if (fileExists)
+            {
+                Console.WriteLine("File already exists. Please choose a different name.");
+            }
+        } while (fileExists);
+
+        fileName = fileName + ".txt";
+
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                foreach (int number in numbers)
+                {
+                    string data = number.ToString();
+                    writer.WriteLine(data);
+                }
+            }
+
+            string fullPath = Path.GetFullPath(fileName);
+            Console.WriteLine($"Data has been written to {fullPath}");
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"An error occurred while writing the file: {e.Message}");
+        }
+    }
+}
---
+++ KP-Lab2/Lab2/Program.cs
@@ -0,0 +1,40 @@
+﻿using Lab2.Helpers;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter path to file: ");
+        string filePath = Console.ReadLine();
+
+        try
+        {
+            string fileContent = FileManagement.GetDataFromFile(filePath);
+
+            if (string.IsNullOrEmpty(fileContent))
+                throw new CustomException("File Is Empty");
+
+            var incomeData = MainLogic.GetIncomeData(fileContent);
+            MainLogic.NumbersCheck(incomeData);
+
+            var createdArray = MainLogic.CreateMainArray(incomeData);
+
+            var result = MainLogic.DeletedSeconds(incomeData, createdArray);
+
+            FileManagement.WriteAnswer(result);
+
+            Console.WriteLine("Completed");
+
+        }
+        catch (CustomException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex.Message);
+            Console.ResetColor();
+        }
+    }
+}
